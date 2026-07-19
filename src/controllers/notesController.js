d8879d9ee91147f8af5378d81c1ff7dcf5597a72
Alona Volvach <a.volvach.dev@gmail.com>
2026-07-19 23:44:20 +0200

    update controllers

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -6,23 +6,27 @@ import createHttpError from 'http-errors';
 export const getAllNotes = async (req, res) => {
   const { page = 1, perPage = 10, tag, search } = req.query;
 
-  const skip = (page - 1) * perPage;
+  const skip = (Number(page) - 1) * Number(perPage);
 
-  const notesQuery = Note.find();
+  const filter = {};
 
   if (tag) {
-    notesQuery.where('tag').equals(tag);
+    filter.tag = tag;
   }
+
   if (search) {
-    notesQuery.where({ $text: { $search: search } });
+    filter.$or = [
+      { title: { $regex: search, $options: 'i' } },
+      { content: { $regex: search, $options: 'i' } },
+    ];
   }
 
   const [totalNotes, notes] = await Promise.all([
-    notesQuery.clone().countDocuments(),
-    notesQuery.skip(skip).limit(perPage),
+    Note.countDocuments(filter),
+    Note.find(filter).skip(skip).limit(Number(perPage)),
   ]);
 
-  const totalPages = Math.ceil(totalNotes / perPage);
+  const totalPages = Math.ceil(totalNotes / Number(perPage));
 
   res.status(200).json({
     page: Number(page),
