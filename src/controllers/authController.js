eda575d53c4d82e45655ac2046fdb73d75fff0d9
Alona Volvach <a.volvach.dev@gmail.com>
2026-07-31 01:07:07 +0200

    hw5 mail and img

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,6 +3,12 @@ import bcrypt from 'bcrypt';
 import { User } from '../models/user.js';
 import { createSession, setSessionCookies } from '../services/auth.js';
 import { Session } from '../models/session.js';
+import handlebars from 'handlebars';
+import path from 'node:path';
+import fs from 'node:fs/promises';
+import jwt from 'jsonwebtoken';
+import { sendEmail } from '../utils/sendMail.js';
+
 
 export const registerUser = async (req, res, next) => {
   const { email, password } = req.body;
@@ -51,21 +57,19 @@ export const loginUser = async (req, res, next) => {
 
 export const logoutUser = async (req, res) => {
   const { sessionId } = req.cookies;
-
   if (sessionId) {
     await Session.deleteOne({ _id: sessionId });
   }
 
-  res.clearCookie('sessionId');
   res.clearCookie('accessToken');
   res.clearCookie('refreshToken');
+  res.clearCookie('sessionId');
 
   res.status(204).end();
 };
 
 export const refreshUserSession = async (req, res, next) => {
   const { sessionId, refreshToken } = req.cookies;
-
   const session = await Session.findOne({
     _id: sessionId,
     refreshToken,
@@ -76,34 +80,94 @@ export const refreshUserSession = async (req, res, next) => {
     return;
   }
 
-  const isRefreshTokenExpired =
-    new Date() > session.refreshTokenValidUntil;
-
- if (isRefreshTokenExpired) {
-  await Session.deleteOne({
-    _id: sessionId,
-    refreshToken,
-  });
-
-  res.clearCookie('sessionId');
-  res.clearCookie('accessToken');
-  res.clearCookie('refreshToken');
-
-  return next(createHttpError(401, 'Refresh token expired'));
-}
-
   const userId = session.userId;
 
-  await Session.deleteOne({
-    _id: sessionId,
-    refreshToken,
-  });
+  const isRefresheTokenExpired = new Date() > session.refreshTokenValidUntil;
+
+  if (isRefresheTokenExpired) {
+    next(createHttpError(401, 'Session token expired'));
+    return;
+  }
+
+  await Session.deleteOne({ _id: sessionId, refreshToken });
 
   const newSession = await createSession(userId);
 
   setSessionCookies(res, newSession);
 
+  res.status(200).json({ message: 'Session refreshed' });
+};
+
+export const requestResetEmail = async (req, res, next) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(200).json({
+      message: 'Password reset email sent successfully',
+    });
+    return;
+  }
+
+  const resetToken = jwt.sign(
+    { sub: user._id, email },
+    process.env.JWT_SECRET,
+    { expiresIn: '15m' },
+  );
+
+  const templatePath = path.resolve('src/templates/reset-password-email.html');
+
+  const templateSource = await fs.readFile(templatePath, 'utf-8');
+
+  const template = handlebars.compile(templateSource);
+
+  const html = template({
+    name: user.username,
+    link: `${process.env.FRONTEND_DOMAIN}/reset-password?token=${resetToken}`,
+  });
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_FROM,
+      to: email,
+      subject: 'Reset your password',
+
+      html,
+    });
+  } catch {
+    throw createHttpError(
+      500,
+      'Failed to send the email, please try again later.',
+    );
+  }
+
   res.status(200).json({
-    message: 'Session refreshed',
+    message: 'Password reset email sent successfully',
+  });
+};
+
+export const resetPassword = async (req, res) => {
+  const { token, password } = req.body;
+
+  let payload;
+  try {
+    payload = jwt.verify(token, process.env.JWT_SECRET);
+  } catch {
+    throw createHttpError(401, 'Invalid or expired token');
+  }
+
+  const user = await User.findOne({ _id: payload.sub, email: payload.email });
+  if (!user) {
+    throw createHttpError(404, 'User not found');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  await User.updateOne({ _id: user._id }, { password: hashedPassword });
+
+  await Session.deleteMany({ userId: user._id });
+
+  res.status(200).json({
+    message: 'Password reset successfully',
   });
 };
